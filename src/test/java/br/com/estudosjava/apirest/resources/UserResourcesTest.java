7892a253b3d939628673e9c27fc6b0a374bb2549
Bruno <rexon300008@gmail.com>
2025-02-24 13:52:12 -0300

    Feat: Teste para a endpoint metodo update

--- src/test/java/br/com/estudosjava/apirest/resources/UserResourcesTest.java
+++ src/test/java/br/com/estudosjava/apirest/resources/UserResourcesTest.java
@@ -100,7 +100,21 @@ class UserResourcesTest {
     }
 
     @Test
-    void update() {
+    void quandoAlteradoRetorneSucesso() {
+        when(service.update(userDTO)).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resources.update(ID, userDTO);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NOME, response.getBody().getNome());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(SENHA, response.getBody().getSenha());
     }
 
     @Test
